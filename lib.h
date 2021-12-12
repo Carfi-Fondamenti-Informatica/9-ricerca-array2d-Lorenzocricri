e9b07f8d183ac723429f8b359af8905e8eb35b5c
Lorenzocricri <91842642+Lorenzocricri@users.noreply.github.com>
2021-12-12 13:38:15 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,5 +1,5 @@
-#ifndef INC_8_LIB_H
-#define INC_8_LIB_H
-float numM (const float num[], int dim);
-
-#endif //INC_8_LIB_H
+#ifndef REP_9_RICERCA_ARRAY2D
+#define REP_9_RICERCA_ARRAY2D
+//
+#endif //REP_9_RICERCA_ARRAY2D_LIB_H
+bool res (char[][20],char[],int&);
